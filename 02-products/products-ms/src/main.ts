57de485c4893f15092b1d7d20fad3deed845017f
Javier Flores <jffc.dev@gmail.com>
2025-07-09 06:08:28 -0500

    feat: installing and configuring prisma

--- 02-products/products-ms/src/main.ts
+++ 02-products/products-ms/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('main');
+
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
@@ -11,5 +13,6 @@ async function bootstrap() {
     }),
   );
   await app.listen(process.env.PORT ?? 3000);
+  logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
