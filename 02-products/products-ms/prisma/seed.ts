a994c13beb5ab6cf2ebf342e2d208e4269ec02b2
Javier Flores <jffc.dev@gmail.com>
2025-07-10 00:48:34 -0500

    feat: migrating prisma version and adding seed

--- 02-products/products-ms/prisma/seed.ts
+++ 02-products/products-ms/prisma/seed.ts
@@ -1,11 +1,12 @@
 import { readFileSync } from 'fs';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from './../generated/prisma';
 import { join } from 'path';
 
 const prisma = new PrismaClient();
 
 async function main() {
   const sql = readFileSync(join(__dirname, 'products.sql'), 'utf8');
+  await prisma.product.deleteMany();
   await prisma.$executeRawUnsafe(sql);
 }
 
